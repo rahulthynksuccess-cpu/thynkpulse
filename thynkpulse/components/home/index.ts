8a50686b422f0723297ebfe6677dd3d934d6e0e9
rahulthynksuccess-cpu <rahul.thynksuccess@gmail.com>
2026-03-30 01:18:44 +0530

    Delete thynkpulse directory

--- thynkpulse/components/home/index.ts
+++
@@ -1,3 +0,0 @@
-export { MarqueeSection, StatsBar, CommunitySection, ProfileSection, CTASection } from './HomeSections'
-export { PostsSection, TrendingSection } from './PostsAndTrending'
-export { HeroSection } from './HeroSection'
